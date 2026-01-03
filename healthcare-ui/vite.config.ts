495fa8dc4bde916bb3d64211a2b838ea0a842bac
Ronith Raj <ronithrajkopperla@gmail.com>
2026-01-03 21:13:21 +0530

    changes in vite.config for deployment.

--- healthcare-ui/vite.config.ts
+++ healthcare-ui/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // ensures assets resolve correctly on Vercel
+  build: {
+    outDir: "dist", // Vercel expects dist/
+  },
 });
